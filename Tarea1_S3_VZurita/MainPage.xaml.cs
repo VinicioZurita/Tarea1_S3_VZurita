6d945e7ad2e23276c694201222061a7d79ac18a0
CÉSAR VINICIO ZURITA TORO <cvzurita2@utpl.edu.ec>
2021-04-25 22:45:42 -0500

    Agregar archivos de proyecto.

---
+++ Tarea1_S3_VZurita/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Tarea1_S3_VZurita
+{
+    public partial class MainPage : ContentPage
+    {
+       
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnIngresar_Clicked(object sender, EventArgs e)
+        {
+            string usuario = txtUsuario.Text;
+            string clave = txtClave.Text;
+            if (usuario == "estudiante2021" && clave== "uisrael2021") 
+                await Navigation.PushAsync(new notas());
+            else
+                await DisplayAlert("Advertencia", "¡Usuario o clave incorrectas!", "OK");
+        }
+    }
+}
---
+++ Tarea1_S3_VZurita/notas.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Tarea1_S3_VZurita
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class notas : ContentPage
+    {
+        double suma1, suma2, NF;
+        public notas()
+        {
+            InitializeComponent();
+        }
+        private void btnSumar_Clicked(object sender, EventArgs e)
+        {
+
+            try
+            {//acciones del programa
+
+                if (String.IsNullOrWhiteSpace(txtNotaS1.Text) || String.IsNullOrWhiteSpace(txtNotaE1.Text))
+                {
+                    DisplayAlert("Advertencia", "Los campos (*) son obligatorios.", "OK");
+                }
+                else
+                {
+                    double dato1 = Convert.ToDouble(txtNotaS1.Text);
+                    double dato2 = Convert.ToDouble(txtNotaE1.Text);
+                    suma1 = dato1 * 0.30 + dato2 * 0.20;
+                    txtNP1.Text = suma1.ToString();
+                    if (String.IsNullOrWhiteSpace(txtNotaS2.Text) || String.IsNullOrWhiteSpace(txtNotaE2.Text))
+                    {
+                        DisplayAlert("Advertencia", "Los campos (*) son obligatorios.", "OK");
+                    }
+                    else
+                    {
+                        double dato3 = Convert.ToDouble(txtNotaS2.Text);
+                        double dato4 = Convert.ToDouble(txtNotaE2.Text);
+                        suma2 = dato3 * 0.30 + dato4 * 0.20;
+                        txtNP2.Text = suma2.ToString();
+                        NF = suma1 + suma2;
+                        txtNF.Text = NF.ToString();
+                        if (NF >= 7)
+                            Estado.Text = "APROBADO";
+                        else
+                            if (NF >= 5 && NF < 7)
+                            Estado.Text = "COMPLEMENTARIO";
+                        else
+                            Estado.Text = "REPROBADO";
+                    }
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Alerta", ex.Message, "OK");
+            }
+        }
+    }
+}
